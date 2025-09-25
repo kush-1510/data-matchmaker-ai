16382b89fb0368acd22cb5e05dcdab8c1f51917e
kush-1510 <kushagra.g@atlan.com>
2025-09-25 18:38:17 +0530

    chore: update footer and meta tags

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 export const Footer: React.FC = () => {
   return (
     <footer className="py-8 text-center">
-      <p className="text-muted-foreground">
-        Want to see this match in action?{' '}
+      <p className="text-muted-foreground flex justify-center items-end">
+        Made with ❤️ by {' '}
         <a 
-          href="https://atlan.com/regovern/?ref=/regovern-quiz" 
+          href="https://atlan.com/?ref=/regovern-quiz" 
           target="_blank" 
           rel="noopener noreferrer"
           className="text-primary hover:underline font-medium"
         >
-          Join us at Re:Govern →
+          <img src="https://website-assets.atlan.com/img/atlan-blue.svg" className="ml-2 mb-1" alt="Atlan logo" width={70} height={100} />
         </a>
       </p>
     </footer>
